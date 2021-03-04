33a08050be9eb08d7349db36db4613dd114bee9c
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-03 21:05:50 -0800

    Adding SearchForm component

---
+++ client/src/components/SearchForm/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function SearchForm(props) {
+
+  return (
+    <form className="row border m-5 p-5" onSubmit={props.handleSubmit}>
+      <div className="col-12 mb-3">
+        <h3>Book Search</h3>
+      </div>
+      <div className="col-12">
+        <input type="text" className="form-control" placeholder="Search by title or author" 
+          value={props.value}
+          onChange={props.handleInputChange}
+        />
+      </div>
+    </form>
+  )
+}
+
+export default SearchForm;
