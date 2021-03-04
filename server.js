293f65cb2b0ff68bfd78a31c3d71369e82f2df80
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-03 23:26:23 -0800

    deploying

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooksDB");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooksDB",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 // Start the API server
 app.listen(PORT, function() {
