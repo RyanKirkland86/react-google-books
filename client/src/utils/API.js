e59a32597eae6eecdf9469eb57388efcc940d845
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-03 20:55:13 -0800

    Adding utils

---
+++ client/src/utils/API.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export default {
+  // Get all books from database.
+  getBooks: () => {
+    return axios.get('/api/books');
+  },
+  // Save book data to database.
+  saveBook: data => {
+    return axios.post('/api/books', data);
+  },
+  // Delete book from database.
+  deleteBook: id => {
+    return axios.delete('/api/books/' + id);
+  },
+  // Search books by search term.
+  searchBooks: name => {
+    return axios.get('https://www.googleapis.com/books/v1/volumes?q=' + name)
+  },
+  // Search for single book.
+  searchBook: id => {
+    return axios.get('https://www.googleapis.com/books/v1/volumes/' + id)
+  }
+}
